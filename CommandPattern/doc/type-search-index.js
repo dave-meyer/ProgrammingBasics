fc3f46df4238c9288715e368914b2bdbaa4550d1
dave-meyer <33990571+dave-meyer@users.noreply.github.com>
2018-04-20 12:40:15 +0200

    CommandPattern Javadoc
    (German) Added compiled Javadoc for Command Pattern

---
+++ CommandPattern/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"control","l":"CommandRecorder"},{"p":"control","l":"Figur"},{"p":"control","l":"HoeherCmd"},{"p":"control","l":"ICommand"},{"p":"control","l":"Main"},{"p":"control","l":"TieferCmd"},{"p":"control","l":"VorCmd"}]
